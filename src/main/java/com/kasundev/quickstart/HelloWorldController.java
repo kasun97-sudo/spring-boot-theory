e07ad3adedbffbbc5bec2ed7c4dbfef230e756ef
kasun thenuwara <iamkasun97@gmail.com>
2024-10-06 18:50:54 +0530

    initial commit

---
+++ src/main/java/com/kasundev/quickstart/HelloWorldController.java
@@ -0,0 +1,12 @@
+package com.kasundev.quickstart;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @GetMapping(path = "/hello")
+    public String helloworld(){
+        return "Hello World";
+    }
+}
---
+++ src/main/java/com/kasundev/quickstart/QuickstartApplication.java
@@ -0,0 +1,14 @@
+package com.kasundev.quickstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickstartApplication {
+
+	// 41.50
+	public static void main(String[] args) {
+		SpringApplication.run(QuickstartApplication.class, args);
+	}
+
+}
