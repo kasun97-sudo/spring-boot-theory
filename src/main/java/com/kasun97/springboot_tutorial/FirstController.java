6124571f7d8bd75d02c0fa93df69ecb6013e9c4c
kasun thenuwara <iamkasun97@gmail.com>
2024-10-16 14:31:39 +0530

    path variable and param variable added

--- src/main/java/com/kasun97/springboot_tutorial/FirstController.java
+++ src/main/java/com/kasun97/springboot_tutorial/FirstController.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class FirstController {
-    @ResponseStatus(HttpStatus.ACCEPTED)
-    @GetMapping("/hello")
-    public String sayHello(){
-        return "hello from my first controller";
-    }
+//    @ResponseStatus(HttpStatus.ACCEPTED)
+//    @GetMapping("/hello")
+//    public String sayHello(){
+//        return "hello from my first controller";
+//    }
 
     @PostMapping("/post")
     public String post(
@@ -30,5 +30,22 @@ public class FirstController {
             @RequestBody OrderRecord order
     ){
         return "request accepted and order is "+order.toString();
+    }
+
+    //hello/kasun
+    @GetMapping("/hello/{user-name}")
+    public String pathVar(
+            @PathVariable("user-name") String userName
+    ){
+        return "my value "+userName;
+    }
+
+    //hello?param_name=kasun&param_name2=kiki
+    @GetMapping("/hello")
+    public String paramVar(
+            @RequestParam("user-name") String userName,
+            @RequestParam("user-name-1") String userName2
+    ){
+        return "my value "+userName+" "+userName2;
     }
 }
